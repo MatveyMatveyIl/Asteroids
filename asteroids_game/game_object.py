a9ece91319e0c3acdac24e9287b9da3f5b8f6292
MatveyMatveyIl <matvey.ilichev@gmail.com>
2021-10-20 19:02:47 +0500

    upd game obj class

--- asteroids_game/game_object.py
+++ asteroids_game/game_object.py
@@ -2,14 +2,23 @@ from pygame.math import Vector2
 
 
 class GameObject:
-    def __init__(self, sprite, position: Vector2, velocity: Vector2):
+    def __init__(self, sprite, position: Vector2, velocity: Vector2, screen_size):
         self.sprite = sprite
         self.position = position
         self.velocity = velocity
         self.radius = sprite.get_width() / 2
+        self.screen_size = screen_size
 
     def move(self):
         self.position += self.velocity
+        if self.position.x < 0:
+            self.position.x = self.screen_size[0]
+        elif self.position.x > self.screen_size[0]:
+            self.position.x = 0
+        if self.position.y < 0:
+            self.position.y = self.screen_size[1]
+        elif self.position.y > self.screen_size[1]:
+            self.position.y = 0
 
     def collision(self, other_object):
         dist = self.position.distance_to(other_object.position)
--- asteroids_game/player_ship.py
+++ asteroids_game/player_ship.py
@@ -5,15 +5,13 @@ from asteroids_game.src.image_for_sprites_loader import load_sprite
 
 
 class PlayerShip(GameObject):
-
     def __init__(self, position, screen_size):
-        super().__init__(load_sprite('player_ship'), position, Vector2(0, 0))
+        super().__init__(load_sprite('player_ship'), position, Vector2(0, 0), screen_size)
         self.rotate_angle = 5
         self.direction = Vector2(0, -1)
         self.default_direction = Vector2(0, -1)
         self.sign = {True: 1, False: -1}
         self.acceleration = 0.1
-        self.screen_size = screen_size
 
     def rotate_ship(self, hourly):
         self.direction.rotate_ip(self.rotate_angle * self.sign[hourly])
@@ -30,13 +28,3 @@ class PlayerShip(GameObject):
         else:
             self.velocity = Vector2(self.velocity.x / 1.001, self.velocity.y / 1.001)
 
-    def move(self):
-        self.position += self.velocity
-        if self.position.x < 0:
-            self.position.x = self.screen_size[0]
-        elif self.position.x > self.screen_size[0]:
-            self.position.x = 0
-        if self.position.y < 0:
-            self.position.y = self.screen_size[1]
-        elif self.position.y > self.screen_size[1]:
-            self.position.y = 0
