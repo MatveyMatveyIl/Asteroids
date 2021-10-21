0d7f60ba89d7e570af5f5b085ec2bb9ab5e90151
iulpv <yul_karpova@mail.ru>
2021-10-21 17:32:41 +0500

    handle die

--- asteroids_game/game.py
+++ asteroids_game/game.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 from pygame.math import Vector2
 from asteroids_game.src.image_for_sprites_loader import load_sprite
@@ -46,16 +48,17 @@ class Game:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 # menu
                 pass
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and self.player_ship:
                 self.player_ship.shoot()
 
         pressed_key = pygame.key.get_pressed()
-        if pressed_key[pygame.K_RIGHT] or pressed_key[pygame.K_d]:
-            self.player_ship.rotate_ship(hourly=True)
-        if pressed_key[pygame.K_LEFT] or pressed_key[pygame.K_a]:
-            self.player_ship.rotate_ship(hourly=False)
-        if pressed_key[pygame.K_UP] or pressed_key[pygame.K_w]:
-            self.player_ship.accelerate()
+        if self.player_ship:
+            if pressed_key[pygame.K_RIGHT] or pressed_key[pygame.K_d]:
+                self.player_ship.rotate_ship(hourly=True)
+            if pressed_key[pygame.K_LEFT] or pressed_key[pygame.K_a]:
+                self.player_ship.rotate_ship(hourly=False)
+            if pressed_key[pygame.K_UP] or pressed_key[pygame.K_w]:
+                self.player_ship.accelerate()
 
     def _process_game_logic(self):
         for game_obj in self._get_all_moving_obg():
@@ -72,12 +75,15 @@ class Game:
         self.fps.tick(60)
 
     def _get_all_moving_obg(self):
-        return [self.player_ship, *self.asteroids, *self.bullets]
+        moving_obj = [*self.asteroids, *self.bullets]
+        if self.player_ship is not None:
+            moving_obj.append(self.player_ship)
+        return moving_obj
 
     def _create_asteroids(self):
         for i, count in enumerate(self.levels[self.current_level]):
             self.asteroids += [Asteroid(create_random_position(
-                self.screen.get_width(), self.screen.get_height(), 350, self.player_ship.position
+                self.screen.get_width(), self.screen.get_height(), random.randint(250, 350), self.player_ship.position
             ), self.screen_size, 3 - i, self.asteroids.append) for _ in range(count)]
 
     def _delete_bullets_out_of_screen(self):
@@ -97,10 +103,12 @@ class Game:
                     break
 
     def _check_ship_and_asteroid_collisions(self):
-        for asteroid in self.asteroids:
-            if self.player_ship.collision(asteroid):
-                self.asteroids.remove(asteroid)
-                self.lives -= 1
-                break
-            if self.lives <= 0:
-                pass
+        if self.player_ship is not None:
+            for asteroid in self.asteroids:
+                if self.player_ship.collision(asteroid):
+                    self.asteroids.remove(asteroid)
+                    self.lives -= 1
+                    break
+                if self.lives <= 0:
+                    self.player_ship = None
+                    break
